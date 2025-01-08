3bc43cf027acd7c11fa3c3c2506ecaf84bef38ad
juliobrige <juliobenjamimbernardo36@gmail.com>
2025-01-08 09:40:30 +0200

    julio

---
+++ models/model.py
@@ -0,0 +1,12 @@
+from sqlmodel import Field, SQLModel, create_engine
+from typing import Optional
+from datetime import date
+from decimal import Decimal
+
+class Subscription(SQLModel, table=True):
+    id: int = Field(primary_key=True)
+    empresa: str
+    site: Optional[str] = None
+    data_assinatura: date
+    valor: Decimal
+
